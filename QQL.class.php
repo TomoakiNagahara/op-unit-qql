6fe4549a09cc793bd32b7ecd3904411ff5400c47
Tomoaki imac5k-1 <tomoaki.nagahara@gmail.com>
2019-06-21 15:02:49 +0900

    New: QQL

---
+++ QQL.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * unit-qql:/index.php
+ *
+ * @creation  2019-06-21
+ * @version   1.0
+ * @package   unit-qql
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @creation  2019-06-21
+ */
+namespace OP\UNIT;
+
+/** Used class.
+ *
+ */
+use OP\OP_CORE;
+use OP\OP_UNIT;
+use OP\IF_UNIT;
+
+/** qql
+ *
+ * @creation  2019-06-21
+ * @version   1.0
+ * @package   unit-qql
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class QQL implements IF_UNIT
+{
+	/** trait.
+	 *
+	 */
+	use OP_CORE, OP_UNIT;
+}
