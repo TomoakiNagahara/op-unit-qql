b762e56df0d9b7e853dbfd163ee3c590308d2c61
Tomoaki imac5k-1 <tomoaki.nagahara@gmail.com>
2019-06-21 14:33:16 +0900

    New: LICENSE, index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-qql:/index.php
+ *
+ * @creation  2019-06-21
+ * @version   1.0
+ * @package   unit-qql
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/QQL.class.php');
